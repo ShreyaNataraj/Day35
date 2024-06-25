76f28ad3c6a7ad3ffd2d42800242f61ac56f8a56
Shreya N <nshreya094@gmail.com>
2024-06-25 22:18:46 +0530

    Roman to int

---
+++ ROMAN.py
@@ -0,0 +1,16 @@
+def roman_to_int(s):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev_value = 0
+    for char in reversed(s):
+        value = roman[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+    return total
+
+# Example
+s = "MCMXCIV"
+print(roman_to_int(s))  # Output: 1994
